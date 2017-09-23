5197da5be268d452af2952cfdc5168e05927ecf9
sokol69 <sokolov333d@gmail.com>
2017-09-23 11:49:28 +0300

    hm7 correct

--- src/cookie.js
+++ src/cookie.js
@@ -39,8 +39,85 @@ let addValueInput = homeworkContainer.querySelector('#add-value-input');
 let addButton = homeworkContainer.querySelector('#add-button');
 let listTable = homeworkContainer.querySelector('#list-table tbody');
 
+
+
+function createCookie(name, value) {
+	document.cookie = name + '=' + value;
+}
+
+function deleteCookie(name) {
+    var cookieDate= new Date();
+    cookieDate.setTime(cookieDate.getTime() - 1);
+    document.cookie = name + "=; expires=" + cookieDate.toGMTString();
+}
+
+function isMatching(full, chunk) {
+    full = full.toLowerCase();
+    chunk = chunk.toLowerCase();
+    if (~full.indexOf(chunk)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function ccit() {
+	createCookie(addNameInput.value, addValueInput.value);
+	var tr = document.createElement('tr');
+	var td1 = document.createElement('td');
+	var td2 = document.createElement('td');
+	var td3 = document.createElement('td');
+	var delButton = document.createElement('button');
+
+	td1.className = 'cookies';
+	td2.className = 'cookies';
+	tr.className = 'cookies';
+
+	delButton.innerHTML = 'delete';
+	td1.innerText = addNameInput.value;
+	td2.innerText = addValueInput.value;
+
+	listTable.appendChild(tr);
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	tr.appendChild(td3);
+	td3.appendChild(delButton);
+
+	delButton.addEventListener('click', function() {
+		deleteCookie(td1.innerText);
+		tr.innerHTML = '';
+	});
+}
+
+
+
 filterNameInput.addEventListener('keyup', function() {
+	var cookTr = document.querySelectorAll('tr.cookies');
+	var cookStr = document.cookie;
+	var cookArr = [];
+	cookArr = cookStr.split('; ');
+	listTable.innerHTML = '';
+
+	for (var i = 0; i < cookArr.length; i++) {
+		if (isMatching(cookArr[i], filterNameInput.value)) {
+			var tr = document.createElement('tr');
+			var td = document.createElement('td');
+			td.innerText = cookArr[i];
+
+			listTable.appendChild(tr);
+			tr.appendChild(td);
+		}
+	}
 });
 
+
 addButton.addEventListener('click', () => {
+	var arrTd = document.querySelectorAll('td');
+	for (var i = 0; i < arrTd.length; i++) {
+	if (arrTd[i].innerText === addNameInput.value) {
+		arrTd[i+1].innerText = addValueInput.value;
+		return arrTd;
+	}
+}
+	ccit();
 });
